55148278ed0307cf50ec44c20e83f2e17c16edca
afridayk9 <99346691+afridayk9@users.noreply.github.com>
2024-04-02 07:50:15 -0400

    fin

---
+++ CreditCard_Payment_Strategy/CreditCard_Payment_Strategy/Data/CreditCardModal.cs
@@ -0,0 +1,22 @@
+﻿namespace CreditCard_Payment_Strategy.Data
+{
+    public class CreditCardModal
+    {
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string card_name { get; set; }
+        public double card_limit { get; set; }
+        public double ten_percent_limit { get { return card_limit * 0.1; } }
+        public double thirty_percent_limit { get { return card_limit * 0.3; } }
+        public double current_balance { get; set; }
+        public double percent_utilized => card_limit != 0 ? Math.Round((current_balance / card_limit) * 100, 2) : 0;
+
+    }
+
+    public class CreditCardResponse
+    {
+        public CreditCardModal[] credit_cards { get; set; }
+    }   
+}
+
---
+++ CreditCard_Payment_Strategy/CreditCard_Payment_Strategy/Data/ErrorModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CreditCard_Payment_Strategy.Data;
+
+public class ErrorModel
+{
+    public string Error { get; set; }
+}
